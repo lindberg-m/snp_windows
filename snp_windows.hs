f4d4f6b4d1172759401c6db4981304a06b23e7b6
Markus Lindberg <markus.lindberg89@gmail.com>
2016-02-10 14:20:33 +0100

    defined, but not tested, function alignSnp & alignSnp'

--- snp_windows.hs
+++ snp_windows.hs
@@ -29,27 +29,51 @@ data WindowState a = WindowState {
 data SNP a = SNP {
     chrom  :: Chrom
  ,  pos    :: Pos
- ,  snpDat :: [a]
+ ,  snpDat :: a
 }
 
+--  At the time of initiation, the current chromosome is unknown, thus a 
+--  window-state is not given (Nothing) to the function alignSnp
+--
 -- alignSnp :: Reader WindowConfig (SNP a -> State (WindowState a) [Window a])
 alignSnp :: WindowConfig -> SNP a -> Maybe (WindowState a) -> ((WindowState a), [Window a])
 alignSnp wc snp (Nothing) = let 
   windowState = initWindows (windowSize wc) (windowStep wc) (chrom snp)
-  in align snp windowState
+  in alignSnp' snp windowState
 
 alignSnp wc snp (Just ws)
-  | (chrom snp) == (wsChrom ws) = align snp ws
-  | otherwise = (initWindows (windowSize wc) (windowStep wc) (chrom snp), 
-                 flushActiveWindows ws)
+  | (chrom snp) == (wsChrom ws) = alignSnp' snp ws
+  | otherwise = let
+      windowState          = initWindows (windowSize wc) (windowStep wc) (chrom snp)
+      ret                  = snd $ flushActiveWindows ws
+      (windowState', ret') = alignSnp' snp windowState
+      in (windowState', (ret ++ ret'))
 
 
-align :: SNP a -> WindowState a -> ((WindowState a), [Window a])
-align = undefined
+--align :: Reader WindowConfig (SNP a -> State (WindowState a) [Window a])
+alignSnp' :: SNP a -> WindowState a -> ((WindowState a), [Window a])
+alignSnp' snp ws = let
+  (passed, remaining)     = break downstreamSnp (active ws)
+  (passed', remaining')   = break downstreamSnp (inactive ws)
+  (overlaps, remaining'') = span overlapSnp remaining'
+  newActive               = map updateWindow (remaining ++ overlaps)
+  in (WindowState (wsChrom ws) newActive remaining'', passed ++ passed')
+    where
+      downstreamSnp wndw  = (end wndw) < (pos snp)     --snp is downstream of window 
+      overlapSnp wndw     = ((start wndw) <= (pos snp)) && ((end wndw) >= (pos snp))
+      updateWindow wndw   = Window (start wndw) (end wndw) ((snpDat snp) : wData wndw)
 
 
-flushActiveWindows :: WindowState a -> [Window a]
-flushActiveWindows = undefined
+--flushActiveWindows :: State (WindowState a) [Window a]
+--flushAvtiveWindows =
+--   ws <- get
+--   put $ WindowState (wsChrom ws) [] (inactive ws)
+--   return $ active ws
+flushActiveWindows :: WindowState a -> (WindowState a, [Window a])
+flushActiveWindows ws = let
+  ws' = WindowState (wsChrom ws) [] (inactive ws)
+  ret = active ws
+  in (ws', ret)
 
 data Overlap = Upstream | Downstream | Overlapping
 overlap :: Pos -> Pos -> Pos -> Overlap
